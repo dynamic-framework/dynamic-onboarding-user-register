0677c634a9c54d3a709910cdb37bedf2f2b3c871
Johann Felipe Rojas <johannrip@gmail.com>
2024-04-17 10:49:11 -0500

    style: import dynamic on dev

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,11 @@ import './config/i18nConfig';
 
 import App from './App';
 
-import '@dynamic-framework/ui-react/dist/css/dynamic-ui.css';
-import './styles/base.scss';
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line global-require
+  require('@dynamic-framework/ui-react/dist/css/dynamic-ui.css');
+}
+require('./styles/base.scss');
 
 const root = ReactDOM.createRoot(document.getElementById('userRegister') as Element);
 root.render(
